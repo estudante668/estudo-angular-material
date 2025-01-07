3b46ba5f80e0d2264c22662dc86eed439c2a9dc4
fabricionetto <agostinhonetto73@gmail.com>
2025-01-07 10:11:42 -0300

    sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule, RouterLink],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule, RouterLink, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
