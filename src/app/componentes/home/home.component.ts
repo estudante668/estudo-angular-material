f64ed9ea473c5c308196f5ebf651b1d0f70a4261
fabricionetto <agostinhonetto73@gmail.com>
2025-01-12 21:42:38 -0300

    css-cards

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Dados } from '../dados';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from '../card/card.component';
+import { NgFor } from '@angular/common';
 
 @Component(
   {
   selector: 'app-home',
   standalone: true,
-  imports: [ MatButtonModule, CardComponent],
+  imports: [ MatButtonModule, CardComponent, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -25,35 +26,35 @@ export class HomeComponent {
       modelo: 'Esporte',
      marca:'Adidas',
       preco: '43.98',
-      foto: '/assets/image12.jpg'
+      foto: '/assets/image11.jpg'
     },
     {
       id: 2,
       modelo:'Social',
       marca:'Gabanne',
       preco: '987.98',
-      foto: '/assets/image12.jpg'
+      foto: '/assets/image13.jpg'
     },
     {
       id: 3,
       modelo: 'Sertanejo',
       marca:'Louis',
       preco: '65.98',
-      foto: '/assets/image12.jpg'
+      foto: '/assets/image14.jpg'
     },
     {
       id: 4,
       modelo: 'Inverno',
       marca:'Dinati',
       preco: '453.98',
-      foto: '/assets/image12.jpg'
+      foto: '/assets/image15.jpg'
     },
     {
       id: 5,
       modelo: 'Inverno',
       marca:'Dinati',
       preco: '453.98',
-      foto: '/assets/image12.jpg'
+      foto: '/assets/image16.jpg'
     },
     {
       id: 6,
