943418796dd639adb00b1afda8d8bfcce91fad1c
fabricionetto <agostinhonetto73@gmail.com>
2025-01-15 13:50:06 -0300

    botao

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -3,12 +3,14 @@ import { Dados } from '../dados';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from '../card/card.component';
 import { NgFor } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component(
   {
   selector: 'app-home',
   standalone: true,
-  imports: [ MatButtonModule, CardComponent, NgFor],
+  imports: [ MatButtonModule, CardComponent, NgFor, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
